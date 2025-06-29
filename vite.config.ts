d8de7a3bbef655f149bf22c5ef611f900b9bb0d7
yannick07187 <yannicknaumann@icloud.com>
2025-06-29 21:40:04 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/print-it-amazon/", // WICHTIG für GitHub Pages!
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
