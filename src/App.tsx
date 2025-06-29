54ad4ffd6411efa1be2d93a0d5432c6581f0d4cd
yannick07187 <yannicknaumann@icloud.com>
2025-06-29 21:53:13 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { HashRouter, Routes, Route } from "react-router-dom"; // <-- HashRouter statt BrowserRouter!
 import Layout from "./components/Layout";
 import Homepage from "./pages/Homepage";
 import Upload from "./pages/Upload";
