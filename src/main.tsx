4a512fe360748ac1ffa9e8d8f064eb218d10baef
yannick07187 <yannicknaumann@icloud.com>
2025-06-29 21:40:57 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { HashRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
